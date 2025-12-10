a51064374b53b24fea0577e49524d47c360628d9
BryxOG <143657248+BryxOG@users.noreply.github.com>
2025-12-10 12:43:14 +0300

    Add files via upload

---
+++ Crypto/src/main/java/com/example/web/WebCryptoApplication.java
@@ -0,0 +1,14 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCryptoApplication
+{
+	public static void main(String[] args)
+    {
+		SpringApplication.run(WebCryptoApplication.class, args);
+	}
+
+}
